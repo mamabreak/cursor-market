648350812d4c0ae82d35e25d8a6768dd5c973fba
apathywoke <asdysphoria@gmail.com>
2024-10-09 21:52:40 +0300

    Some changes on /home page.

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../Components/header';
-import styles from './home.module.css'
+import styles from './home.module.css';
+import modern from '../../images/modern.png'
+import anime from '../../images/anime.png'
+import cartoon from '../../images/cartoon.png'
+import empty from '../../images/empty.png'
 
 const Home = () => {
     return (
         <>
         <Header />
-        <div className={`${styles.first_floor} ${styles.justify_center} ${styles.flex_dir_column} ${styles.center_items} ${styles.position_absolute} ${styles.half_black}`}>
+        <div className={`${styles.first_floor} ${styles.justify_center} ${styles.flex_dir_column} ${styles.center_items} ${styles.half_black}`}>
             <div className={` ${styles.first_span_size} ${styles.space_between} ${styles.additional_padding}`}>
                 <span className={`${styles.span_styles} ${styles.white_text}`}>Looking</span>
                 <span className={styles.span_styles}>How about</span>
@@ -26,6 +30,27 @@ const Home = () => {
             <div className={`${styles.fifth_span_size} ${styles.space_between} ${styles.additional_padding}`}>
                 <span className={`${styles.span_styles} ${styles.fifth_span__bigger_size} ${styles.white_text}`}>?</span>
                 <span className={`${styles.span_styles} ${styles.fifth_span__bigger_size}`}>!</span>
+            </div>
+        </div>
+        <div className={`${styles.second_floor} ${styles.flex_dir_column} ${styles.justify_center} ${styles.center_items}`}>
+            <span className={`${styles.heading_text}`}>MANY CATEGORIES</span>
+            <div className={`${styles.categories} ${styles.space_between} ${styles.center_items} ${styles.anti_broke}`}>
+                <div className={styles.image_container}>
+                    <img src={modern} alt="Modern" className={styles.preview_picture} />
+                    <span className={`${styles.common_text} ${styles.picture_text} ${styles.color_black}`}>Modern</span>
+                </div>
+                <div className={styles.image_container}>
+                    <img src={anime} alt="Modern" className={styles.preview_picture} />
+                    <span className={`${styles.common_text} ${styles.picture_text}`}>Anime</span>
+                </div>
+                <div className={styles.image_container}>
+                    <img src={cartoon} alt="Modern" className={styles.preview_picture} />
+                    <span className={`${styles.common_text} ${styles.picture_text}`}>Cartoon</span>
+                </div>
+                <div className={styles.image_container}>
+                    <img src={empty} alt="Modern" className={`${styles.preview_picture}`} />
+                    <span className={`${styles.common_text} ${styles.picture_text} ${styles.color_black} ${styles.coming_soon}`}>More categories is coming soon...</span>
+                </div>
             </div>
         </div>
         </>
